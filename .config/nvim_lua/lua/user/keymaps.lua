d1fd5d7308feba030a297efc26e5a8a79c07b0ff
SENMA <SENMA@MICRON.COM>
2022-01-04 18:19:21 -0600

    finished the most of the function of lua nvim config.

--- .config/nvim_lua/lua/user/keymaps.lua
+++ .config/nvim_lua/lua/user/keymaps.lua
@@ -18,46 +18,63 @@ vim.g.maplocalleader = " "
 --   term_mode = "t",
 --   command_mode = "c",
 
--- * Normal * --
--- Window
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+-- **** Normal **** --
+keymap("n", "wh", "<C-w>h", opts)
+keymap("n", "wj", "<C-w>j", opts)
+keymap("n", "wk", "<C-w>k", opts)
+keymap("n", "wl", "<C-w>l", opts)
 
-keymap("n", "<leader>e", ":Lexplore 30<cr>", opts)
+-- Window
+keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+
+-- Split window
+keymap("n", "ws", ":split<CR>", opts)
+keymap("n", "wv", ":vsplit<CR>", opts)
 
 -- Resize with arrows
-keymap("n", "<C-J>", ":resize +2<CR>", opts)
-keymap("n", "<C-K>", ":resize -2<CR>", opts)
-keymap("n", "<C-H>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-L>", ":vertical resize +2<CR>", opts)
+keymap("n", "<C-k>", ":resize -2<CR>", opts)
+keymap("n", "<C-j>", ":resize +2<CR>", opts)
+keymap("n", "<C-h>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-l>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
--- * Insert * --
--- Remap ESC to jk
-keymap("i", "jk", "<ESC>", opts)
+-- Bbye the buffer
+keymap("n", "wq", ":Bdelete<CR>", opts)
 
--- * Visual * --
+-- Telescope --
+keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<CR>", opts)
+keymap("n", "<leader>rg", "<cmd>Telescope live_grep<CR>", opts)
+keymap("n", "<leader>/", "<cmd>Telescope current_buffer_fuzzy_find<CR>", opts)
+keymap("n", ";", "<cmd>Telescope buffers<CR>", opts)
+
+-- **** Insert **** --
+-- Remap ESC to jj
+keymap("i", "jj", "<ESC>", opts)
+
+-- **** Visual **** --
 -- Copy to the clipboard
-keymap("v", "<C-c>", "*y :let @+=@*<CR>", opts)
+-- keymap("v", "<C-c>", "*y :let @+=@*<CR>", opts)
+--
 
 -- Stay in indent mode
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
 -- Move text up and down
-keymap("v", "<C-f>", ":move '>+1<CR>gv-gv", opts)
-keymap("v", "<C-v>", ":move '<-2<CR>gv-gv", opts)
+keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-k>", ":m .-2<CR>==", opts)
 keymap("v", "p", '"_dP', opts)
 
--- * Visual Block * --
+-- **** Visual Block **** --
 -- Move text up and down
 keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move .+1<CR>==", opts)
+keymap("x", "<A-k>", ":move .-2<CR>==", opts)
 -- Copy to the clipboard
-keymap("x", "<C-c>", "*y :let @+=@*<CR>", opts)
+-- keymap("x", "<C-c>", "*y :let @+=@*<CR>", opts)
+
 
